ae5ebd6b9e2c15d33b0b23deb909070bfdb78c49
talhaahmed09 <endhogaya3@gmail.com>
2020-06-23 10:33:11 +0500

    responsive change

--- slicker.js
+++ slicker.js
@@ -4,7 +4,34 @@
   infinite: true,
   centerMode: true,
   slidesToShow: 3,
-  slidesToScroll: 2,
+  slidesToScroll: 3,
+  useTransform:true,
+  useTransition:true,
+  zIndex: 1000,
+   
+  appendArrows: $('.arrows'),
+  prevArrow:
+  ' <button class="slider-prev "><i class="fa fa-chevron-left"></i> </button>',
+
+  nextArrow:
+  ' <button class="slider-next"><i class="fa fa-chevron-right"></i></button>',
+  mobileFirst: true,
+
+  responsive:[{
+  breakpoints: 640, settings: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    breakpoints: 768, settings: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    breakpoints: 1024, settings: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  }],
+  
   customPaging : function(slider, i) {
 var thumb = $(slider.$slides[i]).data();
 return '<a>'+i+'</a>';
